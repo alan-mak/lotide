0670b64cb8457f46ee362778043580f9d783c7db
Alan <alanmak95@gmail.com>
2020-11-24 23:05:39 +0000

    Function to remove elements from an array using another array

---
+++ without.js
@@ -0,0 +1,34 @@
+let eqArrays = function(arr1, arr2) {
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let assertArraysEqual = function(arr1, arr2) {
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      return console.log(`🔴🔴🔴 false [${arr1}] !== [${arr2}]`);
+    }
+  }
+  return console.log(`👍👍👍 true [${arr1}] === [${arr2}]`);
+};
+
+let without = function(source, itemsToRemove) {
+  let kept = [];
+  for(let item of itemsToRemove) {
+    console.log("To remove", item)
+  }
+  return kept;
+};
+
+// TEST CASES
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
