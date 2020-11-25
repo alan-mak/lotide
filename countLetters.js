8ec8d7e4498c190dd941a1067c412b4846699548
Alan <alanmak95@gmail.com>
2020-11-25 19:59:14 +0000

    Added logic of counting letters individually

--- countLetters.js
+++ countLetters.js
@@ -9,7 +9,13 @@ const assertEqual = function(actual, expected) {
 let countLetters = function(input) {
   let noSpace = input.split(" ").join("");
   let count = {};
-
+  for (let letter of noSpace) {
+    if (count[letter]) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
   return count;
 };
 
