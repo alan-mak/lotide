4e245c7be0d63092ed6d28d13e05fd746d137b31
Alan <alanmak95@gmail.com>
2020-11-25 18:05:08 +0000

    Working case for 1-4 elements in array

--- middle.js
+++ middle.js
@@ -17,15 +17,20 @@ let eqArrays = function(arr1, arr2) {
 };
 
 let middle = function(arr) {
+  let newArr = [];
+  let mid = Math.floor(arr.length / 2);
   if (arr.length === 1 || arr.length === 2) {
-    return []
-  }
+    return newArr;
+  } else if (arr.length % 2 === 1) {
+    newArr.push(arr[mid]);
+  } 
+  return newArr;
 };
 
 // TEST CASES
 console.log(middle([1])); // => []
 console.log(middle([1, 2])); // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
