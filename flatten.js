419fff2ea308417990d7199ac6e031c7e76d81a8
Alan <alanmak95@gmail.com>
2020-11-25 03:08:39 +0000

    Flatten array by 1 level with test case

--- flatten.js
+++ flatten.js
@@ -24,4 +24,4 @@ let flatten = function(input) {
 
 // TEST CASES
 flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6])
