849d11d5beb2c4f5e9d885f119782ed4a31d9474
Alan <alanmak95@gmail.com>
2020-11-25 18:07:27 +0000

    Eslint Fix

--- middle.js
+++ middle.js
@@ -34,7 +34,7 @@ let middle = function(arr) {
 // TEST CASES
 console.log(middle([1])); // => []
 console.log(middle([1, 2])); // => []
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+console.log(middle([1, 2, 3])); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1, 2, 3, 4])); // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
