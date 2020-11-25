32d716e278771191f7ea3583d454ee2bfdad66d4
Alan <alanmak95@gmail.com>
2020-11-25 17:19:27 +0000

    Working case for 1 & 2 elements in array

---
+++ middle.js
@@ -0,0 +1,31 @@
+let assertArraysEqual = function(arr1, arr2) {
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      return console.log("🔴🔴🔴 false");
+    }
+  }
+  return console.log("👍👍👍 true");
+};
+
+let eqArrays = function(arr1, arr2) {
+  for (let index in arr1) {
+    if (arr1[index] !== arr2[index]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+let middle = function(arr) {
+  if (arr.length === 1 || arr.length === 2) {
+    return []
+  }
+};
+
+// TEST CASES
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
+middle([1, 2, 3]) // => [2]
+middle([1, 2, 3, 4, 5]) // => [3]
+middle([1, 2, 3, 4]) // => [2, 3]
+middle([1, 2, 3, 4, 5, 6]) // => [3, 4]
