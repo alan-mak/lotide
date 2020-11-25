ea878a8003b644553acd0023d3257725819b4c8a
Alan <alanmak95@gmail.com>
2020-11-25 19:58:03 +0000

    took away all the input spaces since we are not counting them

---
+++ countLetters.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+let countLetters = function(input) {
+  let noSpace = input.split(" ").join("");
+  let count = {};
+  for (let letter of noSpace) {
+    if (count[letter]) {
+      count[letter] += 1;
+    } else {
+      count[letter] = 1;
+    }
+  }
+  return count;
+};
+
+// TEST CASES
+let counter = countLetters('lhl');
+assertEqual(counter["l"], 2);
+assertEqual(counter["h"], 1)
