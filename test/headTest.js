cfa001f7087fb6acee8332540d4e4dc832b5a15c
Alan <alanmak95@gmail.com>
2020-11-30 20:06:17 +0000

    Fixed with mocha

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,20 @@
 // Head TEST CASES
-console.log("Head");
+const assert = require('chai').assert;
 const head = require('../head');
-const assertEqual = require('../assertEqual');
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), 6);
+
+console.log("Head");
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([5, 2, 3]), 5);
+  });
+  
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5)
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  })
+});
