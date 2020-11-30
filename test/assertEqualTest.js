82d42f35b723b17e4099a294f4a2159902e76385
Alan <alanmak95@gmail.com>
2020-11-30 19:43:29 +0000

    Update

---
+++ test/assertEqualTest.js
@@ -0,0 +1,7 @@
+console.log("Assert Equal");
+const assertEqual = require('../assertEqual');
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello", "hello");
+assertEqual(5, 13);
+console.log("-----------------------");
